08ef54d58afcc9a5d93608845df26fac7cb03a55
Tong Chen <tong@smail.nju.edu.cn>
2021-03-04 11:29:07 +0800

    CLIC toolbox

---
+++ code/Util/CLIC/compare.py
@@ -0,0 +1,7 @@
+## -*- coding: utf-8 -*-
+import metrics
+
+a = {0:"/Users/tong/Downloads/kodak/kodim08.png"}
+b = {0:r"/Users/tong/Documents/科研/Attack/kodak_in.png"}
+results = metrics.evaluate(a,b)
+print(results)
