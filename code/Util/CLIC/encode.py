4bc9e576ee30d945d157d20f7a9bc6830ffe81a5
Tong Chen <tong@smail.nju.edu.cn>
2021-05-19 14:53:35 +0800

    update

--- code/Util/CLIC/encode.py
+++ code/Util/CLIC/encode.py
@@ -3,7 +3,7 @@ import os
 from glob import glob
 
 # CLIC Valid
-images = glob("/data/chentong/CLIC2020_valid/valid/*.png")
+images = sorted(glob("/data/chentong/CLIC2020_valid/valid/*.png"))
 # Kodak
 # images = glob("/data/ljp105/NIC_Dataset/test/ClassD_Kodak/*.png")
 
@@ -11,7 +11,7 @@ for image in images:
     filename = image.split('/')[-1]
     cmd = "python inference.py --encode -i %s -o /output/%s -m_dir /model/chentong/NIC_rctx --block_width 3000 --block_height 3000 -m 0"%(image, filename[:-3]+"bin")
     os.system(cmd)
-    cmd = "python inference.py --decode -i /output/%s -o /output/%s -m_dir /model/chentong/NIC_rctx --block_width 3000 --block_height 3000 -m 0"%(filename[:-3]+"bin", filename[:-4]+"_rec.png")
-    os.system(cmd)
+    # cmd = "python inference.py --decode -i /output/%s -o /output/%s -m_dir /model/chentong/NIC_rctx --block_width 3000 --block_height 3000 -m 0"%(filename[:-3]+"bin", filename[:-4]+"_rec.png")
+    # os.system(cmd)
 
     
